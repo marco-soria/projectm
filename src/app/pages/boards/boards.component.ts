bb1040e60f3bc82f934b1bf9f667c09a9e69d0a8
marco-soria <marco.soriaa7@gmail.com>
2024-08-24 20:38:13 -0500

    dynamic accordeon

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -4,11 +4,12 @@ import { faBox, faWaveSquare, faClock, faAngleUp, faAngleDown, faHeart, faBorder
 import { faTrello } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [NavbarComponent, FontAwesomeModule, CdkAccordionModule],
+  imports: [NavbarComponent, FontAwesomeModule, CdkAccordionModule, CommonModule],
   templateUrl: './boards.component.html'
 })
 export class BoardsComponent {
@@ -23,4 +24,40 @@ export class BoardsComponent {
   faBorderAll = faBorderAll;
   faUsers = faUsers;
   faGear = faGear;
+
+  items = [
+    {
+      label: 'Item 1',
+      items: [
+        {
+          label: 'Sub Item 1.1'
+        },
+        {
+          label: 'Sub Item 1.2'
+        }
+      ]
+    },
+    {
+      label: 'Item 2',
+      items: [
+        {
+          label: 'Sub Item 2.1'
+        },
+      ]
+    },
+    {
+      label: 'Item 3',
+      items: [
+        {
+          label: 'Sub Item 3.1'
+        },
+        {
+          label: 'Sub Item 3.2'
+        },
+        {
+          label: 'Sub Item 3.3'
+        },
+      ]
+    }
+  ];
 }
