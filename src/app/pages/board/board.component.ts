e421a9ae52c5cbe5b209b2f69d4c0914e356afad
marco-soria <marco.soriaa7@gmail.com>
2024-08-24 21:14:05 -0500

    dragandrop and transfer

---
+++ src/app/pages/board/board.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ToDo } from '../../models/todo.model';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-board',
+  standalone: true,
+  imports: [DragDropModule, NavbarComponent, CommonModule],
+  templateUrl: './board.component.html',
+  styles: [
+    `
+    /* Animate items as they're being sorted. */
+    .cdk-drop-list-dragging .cdk-drag {
+      transition: transform 250ms cubic-bezier(0, 0, 0.2, 1);
+    }
+    /* Animate an item that has been dropped. */
+    .cdk-drag-animating {
+      transition: transform 300ms cubic-bezier(0, 0, 0.2, 1);
+    }
+    `
+  ]
+})
+export class BoardComponent {
+
+  todos: ToDo[] = [
+    {
+      id: '1',
+      title: 'Make dishes'
+    },
+    {
+      id: '2',
+      title: 'Buy a unicorn'
+    }
+  ];
+
+  doing: ToDo[] = [
+    {
+      id: '3',
+      title: 'Watch Angular Path in Platzi'
+    }
+  ];
+
+  done: ToDo[] = [
+    {
+      id: '4',
+      title: 'Play video games'
+    }
+  ];
+
+  drop(event: CdkDragDrop<ToDo[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+
+  }
+}
