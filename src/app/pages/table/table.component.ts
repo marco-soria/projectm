c38216f32e7a12f4480e9b7d39ddc7697d1d3e89
marco-soria <marco.soriaa7@gmail.com>
2024-08-24 22:21:53 -0500

    data source

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -4,17 +4,21 @@ import { HttpClient } from '@angular/common/http';
 import { Product } from '../../models/product.model';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { CdkTableModule } from '@angular/cdk/table';
+import { DataSourceProduct } from './data-source';
+import { BtnComponent } from '../../components/btn/btn.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [NavbarComponent, CdkTableModule],
+  imports: [NavbarComponent, CdkTableModule, BtnComponent, CommonModule],
   templateUrl: './table.component.html'
 })
 export class TableComponent {
 
-  products: Product[] = [];
-  columns: string[] = ['cover', '#No', 'Name', 'price'];
+  dataSource = new DataSourceProduct();
+  columns: string[] = ['#No', 'Name', 'price', 'cover', 'actions'];
+  total = 0;
 
   constructor(
     private http: HttpClient
@@ -23,8 +27,13 @@ export class TableComponent {
   ngOnInit(): void {
     this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
     .subscribe(data => {
-      this.products = data;
+      this.dataSource.init(data);
+      this.total = this.dataSource.getTotal();
     })
+  }
+
+  update(product: Product) {
+    this.dataSource.update(product.id, { price: 20 });
   }
 
 }
